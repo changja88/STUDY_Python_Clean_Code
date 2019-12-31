e4458164946eb79cbd65be124ebf05f8a6f5898d
changja88 <DJdnls88!!>
2019-12-31 21:16:45 +0900

    첫 기능 테스트 주석 추가 및 unittest 사용

--- functional_test.py
+++ functional_test.py
@@ -1,7 +1,21 @@
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
+import unittest
 
-browser = webdriver.Chrome(ChromeDriverManager().install())
-browser.get('http://localhost:8000')
 
-assert 'Django' in browser.title
+class NewVistorTest(unittest.TestCase):
+    def setUp(self): # 테스트 시작 때 실행 된다
+        self.browser = webdriver.Chrome(ChromeDriverManager().install())
+        self.browser.implicitly_wait(3) # 페이지 로딩이 끝나고 테스트가 실행되는 것을 확실히 하기 위해서
+
+    def tearDown(self):# 테스트 마지막에 실행 된다(에러가 발생해도 실행된다)
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        self.browser.get('http://localhost:8000')
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test!')
+
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
