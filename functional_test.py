0839beb0b17d4bb38cbdd24bb267f59bca4fa08f
changja88 <DJdnls88!!>
2020-01-02 21:59:43 +0900

    기능 테스트가 작업 아이템을 추가할 수있는지 확인

--- functional_test.py
+++ functional_test.py
@@ -1,20 +1,40 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 from webdriver_manager.chrome import ChromeDriverManager
 import unittest
 
 
 class NewVistorTest(unittest.TestCase):
-    def setUp(self): # 테스트 시작 때 실행 된다
+    def setUp(self):  # 테스트 시작 때 실행 된다
         self.browser = webdriver.Chrome(ChromeDriverManager().install())
-        self.browser.implicitly_wait(3) # 페이지 로딩이 끝나고 테스트가 실행되는 것을 확실히 하기 위해서
+        self.browser.implicitly_wait(3)  # 페이지 로딩이 끝나고 테스트가 실행되는 것을 확실히 하기 위해서
 
-    def tearDown(self):# 테스트 마지막에 실행 된다(에러가 발생해도 실행된다)
+    def tearDown(self):  # 테스트 마지막에 실행 된다(에러가 발생해도 실행된다)
         self.browser.quit()
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get('http://localhost:8000')
         self.assertIn('To-Do', self.browser.title)
+
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            '작업 아이템 입력'
+        )
+        inputbox.send_keys('공작깃털 사기')
+        inputbox.send_keys(Keys.ENTER)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_element_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: 공작깃털 사기' for row in rows)
+        )
+
         self.fail('Finish the test!')
+
 
 
 if __name__ == '__main__':
