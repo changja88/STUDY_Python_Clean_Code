0f807b23cbec853729b772f935346a6717edb85f
changja88 <DJdnls88!!>
2020-01-04 23:35:56 +0900

    save

--- functional_test.py
+++ functional_test.py
@@ -1,7 +1,9 @@
+import time
+import unittest
+
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from webdriver_manager.chrome import ChromeDriverManager
-import unittest
 
 
 class NewVistorTest(unittest.TestCase):
@@ -20,21 +22,18 @@ class NewVistorTest(unittest.TestCase):
         self.assertIn('To-Do', header_text)
 
         inputbox = self.browser.find_element_by_id('id_new_item')
-        self.assertEqual(
-            inputbox.get_attribute('placeholder'),
-            '작업 아이템 입력'
-        )
-        inputbox.send_keys('공작깃털 사기')
+        inputbox.send_keys('공작깃털을 이용해서 그물 만들기')
         inputbox.send_keys(Keys.ENTER)
 
         table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_element_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: 공작깃털 사기' for row in rows),
-            "신규 작업이 테이블에 표시되지 않는다"
+        rows = table.find_elements_by_id('tr')
+        self.assertIn('1: 공작깃털 사기', [row.text for row in rows])
+        self.assertIn(
+            '2: 공작깃털을 이용해서 그물 만들기',
+            [row.text for row in rows]
         )
 
-        self.fail('Finish the test!')
+        self.fail('Finish the Test!')
 
 
 if __name__ == '__main__':
